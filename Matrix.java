e7dae6f4b3e75f378c8d3bfafe85082630967255
Kimhouy HOR <bunret2015@gmail.com>
2026-02-10 19:23:42 +0700

    matrix solution

--- Matrix.java
+++ Matrix.java
@@ -1,19 +1,143 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class Matrix {
-  public void createMatrix() {
+
+  private Scanner scanner;
+
+  Matrix() {
+    scanner = new Scanner(System.in);
+  }
+
+  public void display(ArrayList<ArrayList<Integer>> matrix) {
+
+    System.out.println("Matrix:");
+    for (ArrayList<Integer> row : matrix) {
+      for (Integer value : row) {
+        System.out.print(value + " ");
+      }
+      System.out.println();
+    }
+
+  }
+
+  public boolean validateMatrix(ArrayList<ArrayList<Integer>> matrixA, ArrayList<ArrayList<Integer>> matrixB) {
+    System.out.println("Checking if matrices are created...");
+    if (matrixA == null || matrixB == null) {
+      System.out.println("Matrices are not created yet.");
+      return false;
+    }
+
+    if (matrixA.size() != matrixB.size() || matrixA.get(0).size() != matrixB.get(0).size()) {
+      System.out.println("Matrix addition is not possible: dimensions mismatch.");
+      return false;
+    }
+
+    return true;
+  }
+
+  public ArrayList<ArrayList<Integer>> createMatrix() {
+    int columns, rows;
+    System.out.print("Enter number of columns: ");
+    columns = scanner.nextInt();
+    System.out.print("Enter number of rows: ");
+    rows = scanner.nextInt();
+
+    ArrayList<ArrayList<Integer>> dynamicArray = new ArrayList<>();
+
+    for (int i = 0; i < columns; i++) {
+      ArrayList<Integer> row = new ArrayList<>();
+      for (int j = 0; j < rows; j++) {
+        System.out.print("Enter value for [" + i + "][" + j + "]: ");
+        row.add(scanner.nextInt());
+      }
+      dynamicArray.add(row);
+    }
     System.out.println("Matrix created.");
+
+    display(dynamicArray);
+
+    return dynamicArray;
   }
 
-  public boolean add(){ 
-    return true;
+  public void add(ArrayList<ArrayList<Integer>> matrixA, ArrayList<ArrayList<Integer>> matrixB) {
 
+    if (!validateMatrix(matrixA, matrixB)) {
+      return;
+    }
+
+    int rows = matrixA.size();
+    int columns = matrixB.get(0).size();
+
+    ArrayList<ArrayList<Integer>> resultMatrix = new ArrayList<>();
+
+    for (int i = 0; i < rows; i++) {
+      ArrayList<Integer> row = new ArrayList<>();
+      for (int j = 0; j < columns; j++) {
+        row.add(0);
+      }
+      resultMatrix.add(row);
+    }
+
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < columns; j++) {
+        resultMatrix.get(i).set(j, matrixA.get(i).get(j) + matrixB.get(i).get(j));
+      }
+    }
+
+    display(resultMatrix);
+    System.out.println("Matrices added.");
   }
 
-  public boolean multiply(){ 
-    return true;
+  public void multiply(ArrayList<ArrayList<Integer>> matrixA, ArrayList<ArrayList<Integer>> matrixB) {
 
+    validateMatrix(matrixA, matrixB);
+
+    int rows = matrixA.size();
+    int columns = matrixB.get(0).size();
+
+    ArrayList<ArrayList<Integer>> resultMatrix = new ArrayList<>();
+
+    for (int i = 0; i < rows; i++) {
+      ArrayList<Integer> row = new ArrayList<>();
+      for (int j = 0; j < columns; j++) {
+        row.add(0);
+      }
+      resultMatrix.add(row);
+    }
+
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < columns; j++) {
+        for (int k = 0; k < columns; k++) {
+          resultMatrix.get(i).set(j, resultMatrix.get(i).get(j) + matrixA.get(i).get(k) * matrixB.get(k).get(j));
+        }
+      }
+    }
+
+    display(resultMatrix);
+    System.out.println("Matrices multiplied.");
   }
 
-  public void display() {
-    System.out.println("Matrix displayed.");
+  public ArrayList<ArrayList<Integer>> randomMatrix() {
+
+    int columns, rows;
+    System.out.print("Enter number of columns: ");
+    columns = scanner.nextInt();
+    System.out.print("Enter number of rows: ");
+    rows = scanner.nextInt();
+
+    ArrayList<ArrayList<Integer>> dynamicArray = new ArrayList<>();
+    for (int i = 0; i < columns; i++) {
+      ArrayList<Integer> row = new ArrayList<>();
+      for (int j = 0; j < rows; j++) {
+        row.add((int) (Math.random() * 10)); // Random values between 0 and 9
+      }
+      dynamicArray.add(row);
+    }
+
+    display(dynamicArray);
+
+    return dynamicArray;
   }
+
 }
--- SingleThread.java
+++ SingleThread.java
@@ -1,5 +1,94 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SingleThread {
+
+  private Matrix matrix;
+  private Scanner scanner;
+  private int choice;
+
+  private ArrayList<ArrayList<Integer>> matrixA;
+  private ArrayList<ArrayList<Integer>> matrixB;
+  private int countMatrix;
+
+  SingleThread() {
+    matrix = new Matrix();
+    scanner = new Scanner(System.in);
+    choice = 5;
+    countMatrix = 0;
+  }
+
+  private boolean validateMatrix() {
+    System.out.println("Checking if matrices are created...");
+    if (matrixA == null || matrixB == null) {
+      System.out.println("Matrices are not created yet.");
+      return false;
+    }
+    return true;
+  }
+  
   public void createMenu() {
-    System.out.println("Menu created.");
+
+    do {
+      System.out.println("Menu:");
+      System.out.println("1. Create Matrix");
+      System.out.println("2. Auto Generate Matrices");
+      System.out.println("3. Addition");
+      System.out.println("4. Multiplication");
+      System.out.println("5. Exit");
+
+
+      System.out.print("Enter your choice: ");
+      choice = scanner.nextInt();
+
+      switch (choice) {
+        case 1:
+          if (countMatrix == 0) {
+            matrixA = matrix.createMatrix();
+            countMatrix++;
+          } else if (countMatrix == 1) {
+            matrixB = matrix.createMatrix();
+            countMatrix++;
+          } else {
+            System.out.println("Both matrices are already created.");
+          }
+          break;
+        case 2:
+          if (countMatrix == 0) {
+            matrixA = matrix.randomMatrix();
+            countMatrix++;
+          } else if (countMatrix == 1) {
+            matrixB = matrix.randomMatrix();
+            countMatrix++;
+          } else {
+            System.out.println("Both matrices are already created.");
+          }
+          break;
+        case 3:
+          if (validateMatrix()) {
+            matrix.add(matrixA, matrixB);
+          }
+          break;
+        case 4:
+          if (validateMatrix()) {
+            matrix.multiply(matrixA, matrixB);
+          }
+          break;
+        case  5:
+          System.out.println("Exiting...");
+          scanner.close();
+          return;
+        default:
+          System.out.println("Invalid choice. Please try again.");
+      }
+
+      // wait for user press any key before showing the menu again
+       scanner.nextLine(); 
+
+      // clear screen+
+      // System.out.print("\033[H\033[2J");
+      // System.out.flush();
+
+    } while (choice != 5);
   }
 }
