1e795c3de5ded2a924d60c46fe8092d9870877b6
Kimhouy HOR <bunret2015@gmail.com>
2026-02-10 18:37:43 +0700

    init

---
+++ Matrix.java
@@ -0,0 +1,19 @@
+class Matrix {
+  public void createMatrix() {
+    System.out.println("Matrix created.");
+  }
+
+  public boolean add(){ 
+    return true;
+
+  }
+
+  public boolean multiply(){ 
+    return true;
+
+  }
+
+  public void display() {
+    System.out.println("Matrix displayed.");
+  }
+}
---
+++ SingleThread.java
@@ -0,0 +1,5 @@
+public class SingleThread {
+  public void createMenu() {
+    System.out.println("Menu created.");
+  }
+}
